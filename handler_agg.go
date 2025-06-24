07df43a1c005467c444a285f5c2512fcb7640784
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-24 12:20:53 +0700

    refactor(agg): separate rss feed

---
+++ handler_agg.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerAgg(s *state, cmd command) error {
+	ctx := context.Background()
+	rssFeed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(rssFeed)
+
+	return nil
+}
--- rss_feed.go
+++ rss_feed.go
@@ -68,15 +68,3 @@ func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
 
 	return &rssFeed, nil
 }
-
-func handlerAgg(s *state, cmd command) error {
-	ctx := context.Background()
-	rssFeed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(rssFeed)
-
-	return nil
-}
