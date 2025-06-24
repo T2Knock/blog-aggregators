9f11c6e4a5b0e0845449ba60dbab165bf4a47304
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-24 11:49:01 +0700

    feat(agg): add fetch rss

---
+++ handler_rss.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error making request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return &RSSFeed{}, errors.New("fetching feed failed")
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return &RSSFeed{}, fmt.Errorf("error reading feed body: %w", err)
+	}
+
+	var rssFeed RSSFeed
+	if err := xml.Unmarshal(data, &rssFeed); err != nil {
+		return nil, err
+	}
+
+	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
+	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
+
+	for i, item := range rssFeed.Channel.Item {
+		ptr := &rssFeed.Channel.Item[i]
+		ptr.Title = html.UnescapeString(item.Title)
+		ptr.Description = html.UnescapeString(item.Description)
+	}
+
+	return &rssFeed, nil
+}
+
+func handlerAgg(s *state, cmd command) error {
+	ctx := context.Background()
+	rssFeed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(rssFeed)
+
+	return nil
+}
--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
+	cmds.register("agg", handlerAgg)
 
 	cmd := command{
 		Name:      os.Args[1],
