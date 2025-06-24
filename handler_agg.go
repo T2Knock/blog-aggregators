8db477f2853542139c49d7be27047a4b87031520
knock-huynh <huynhtransongngoc@gmail.com>
2025-06-24 19:33:39 +0700

    feat(scrape): scrape rss feed

--- handler_agg.go
+++ handler_agg.go
@@ -2,17 +2,37 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
+
+	"github.com/T2Knock/blog-aggregators/internal/database"
 )
 
-func handlerAgg(s *state, cmd command) error {
+func scrapeFeeds(s *state, cmd command) error {
 	ctx := context.Background()
-	rssFeed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to fetch next feed: %w", err)
+	}
+
+	fmt.Printf("Fetching %q at %q\n", feed.Name, feed.Url)
+
+	rssFeed, err := fetchFeed(ctx, feed.Url)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(rssFeed)
+	if err = s.db.MarkFeedFetched(ctx, database.MarkFeedFetchedParams{FeedID: feed.FeedID, LastFetchedAt: sql.NullTime{Time: time.Now(), Valid: true}, UpdatedAt: time.Now()}); err != nil {
+		return fmt.Errorf("failed to mark feed fetched: %w", err)
+	}
+
+	fmt.Println("Feed Items")
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("* %s\n", item.Title)
+	}
 
 	return nil
 }
